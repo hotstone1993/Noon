ca7279aeee36905d136b62fe24b03f431b6d6b23
Sinseok Bak <hotstone1993@gmail.com>
2022-04-16 18:01:09 +0900

    구조 변경

---
+++ noon/src/main/cpp/include/Noon.h
@@ -0,0 +1,19 @@
+//
+// Created by newst on 2022-04-16.
+//
+
+#ifndef NOON_NOON_H
+#define NOON_NOON_H
+
+enum {
+    UNKNOWN_ERROR = -1,
+    SUCCESS = 0,
+};
+
+class Noon {
+private:
+    int loadModel(const char* file, size_t fileSize);
+public:
+};
+
+#endif //NOON_NOON_H
