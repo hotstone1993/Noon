0766b491642e424e5fca9ddffe543228d721fca9
Sinseok Bak <hotstone1993@gmail.com>
2022-12-18 04:43:08 +0900

    init test

--- scripts/build.py
+++ scripts/build.py
@@ -5,28 +5,43 @@ import subprocess
 import shutil
 
 BUILD_DIR = '../build'
-BUILD_TYPE = "Release"
+BUILD_TYPE = "Debug"
+Test = "On"
+Target = "Linux"
+
+os.chdir('..')
+if os.path.isdir('./build'):
+    shutil.rmtree('./build')
+os.mkdir('./build')
+os.chdir('./build')
+
+subprocess.call(['conan', 'install', '../', '-if', '.', '--build', 'missing', '-s','build_type=' + BUILD_TYPE])
 
 build_cmd = ["cmake"]
-build_cmd.append("-DANDROID=true")
-build_cmd.append("-DCMAKE_SYSTEM_NAME=Android")
-ARCH_ABI = "arm64-v8a"
-build_cmd.append("-DANDROID_ABI=" + ARCH_ABI)
-build_cmd.append("-DCMAKE_ANDROID_ARCH_ABI=" + ARCH_ABI)
+if (Target == 'Android'):
+    build_cmd.append("-DANDROID=true")
+    build_cmd.append("-DCMAKE_SYSTEM_NAME=Android")
+    ARCH_ABI = "arm64-v8a"
+    build_cmd.append("-DANDROID_ABI=" + ARCH_ABI)
+    build_cmd.append("-DCMAKE_ANDROID_ARCH_ABI=" + ARCH_ABI)
+    if os.environ.get("ANDROID_NDK") is None:
+        build_cmd.append('-DCMAKE_ANDROID_NDK=/mnt/c/Users/newst/AppData/Local/Android/Sdk/ndk/linux-r20b')
+    else:
+        build_cmd.append('-DCMAKE_ANDROID_NDK='+os.environ.get('ANDROID_NDK'))
+elif Target == 'Linux':
+    build_cmd.append("-DCMAKE_SYSTEM_NAME=Linux")
 
-if os.environ.get("ANDROID_NDK") is None:
-    build_cmd.append('-DCMAKE_ANDROID_NDK=/mnt/c/Users/newst/AppData/Local/Android/Sdk/ndk/linux-r20b')
-else:
-    build_cmd.append('-DCMAKE_ANDROID_NDK='+os.environ.get('ANDROID_NDK'))
-build_cmd.append("../")
+if (Test == "On"):
+    build_cmd.append("-DBUILD_TESTING=" + "ON")
 
-if os.path.isdir(BUILD_DIR):
-    shutil.rmtree(BUILD_DIR)
-os.mkdir(BUILD_DIR)
-os.chdir(BUILD_DIR)
+build_cmd.append("-S../")
+build_cmd.append("-B.")
 
 subprocess.call(build_cmd)
-
 print("======start build!!!======")
-
 subprocess.call(['cmake', '--build', '.', '--config', BUILD_TYPE])
+
+if (Test == "On"):
+    print("======start test!!!======")
+    os.chdir('./../bin')
+    subprocess.call(['ctest', '--output-on-failure', '--stop-on-failure', BUILD_TYPE])
